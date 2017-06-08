c6358d29186063c858c4623ebbecff1e97df26d0
leran123 <33862321@qq.com>
2017-06-08 22:49:19 +0800

    listener add or remove emiter

--- main.js
+++ main.js
@@ -23,5 +23,36 @@ fs.readFile('input.txt', function (err, data) {
   console.log(data.toString());
 });
   console.log("end program");
+///////////////////////////////////////////////////////
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
 
+//listener #1 
+var listener1 = function listener1() {
+  console.log('listener1 proc');
+}
+
+//listener2
+var listener2 = function listener2() {
+  console.log('listener2 proc');
+}
+
+eventEmitter.addListener('connection', listener1);
+eventEmitter.on('connection', listener2);
+
+var eventListeners = require('events').EventEmitter.listenerCount(eventEmitter, 'connection');
+console.log(eventEmitter + " times listener connect events");
+
+eventEmitter.emit('connection');
+
+eventEmitter.removeListener('connection', listener1);
+console.log('listener1 never work!');
+
+eventEmitter.emit('connection');
+
+eventListeners = require('events').EventEmitter.listenerCount(eventEmitter, 'connection');
+
+console.log(eventListeners + " listener num");
+
+console.log(" Game over!");
 
