aa7e669c24bc34d6ba28274bbfafbb187c83742c
leran123 <33862321@qq.com>
2017-06-10 14:15:13 +0800

    mnist new

--- mnist_new.py
+++ mnist_new.py
@@ -1,31 +1,11 @@
-import tensorflow as tf
-import numpy as np
-from tensorflow.examples.tutorials.mnist import input_data
-
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-
-x = tf.placeholder(tf.float32, [None, 784])
-w = tf.Variable(tf.zeros([784,10]))
-b = tf.Variable(tf.zeros([10]))
-y = tf.nn.softmax(tf.matmul(x,w) + b)
-y_ = tf.placeholder("float", [None,10])
-cross_entropy = -tf.reduce_sum(y_*tf.log(y))
-
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-init = tf.initialize_all_variables()
-
+import tensorflow as tf 
+import numpy as np 
+from tensorflow.examples.tutorials.mnist import input_data 
+ 
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True) 
+ 
 sess = tf.Session()
-sess.run(init)
 
-for i in range(1000):
-  batch_xs, batch_ys = mnist.train.next_batch(100)
-  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-print sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
-
-sess2 = tf.Session()
 def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.1)
   return tf.Variable(initial)
@@ -39,12 +19,17 @@ def conv2d(x, w):
 
 def max_pool_2x2(x):
   return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], 
-                        strides=[1, 2, 2, 1], padding='SAME')
+                      strides=[1, 2, 2, 1], padding='SAME')
+x = tf.placeholder(tf.float32, [None, 784])
+#w = tf.Variable(tf.zeros([784,10]))
+#b = tf.Variable(tf.zeros([10]))
+#y = tf.nn.softmax(tf.matmul(x,w) + b)
+y_ = tf.placeholder("float", [None,10])
 
 w_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
 
-x_image = tf.reshape(x, [01, 28, 28, 1])
+x_image = tf.reshape(x, [-1, 28, 28, 1])
 
 h_conv1 = tf.nn.relu(conv2d(x_image, w_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
@@ -73,16 +58,17 @@ cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-sess2.run(tf.initialize_all_variables())
+
+sess.run(tf.initialize_all_variables())
 
 for i in range(20000):
   batch = mnist.train.next_batch(50)
   if i%100 == 0:
-    train_accurancy = accuracy.eval(feed_dict={
+    train_accurancy = accuracy.eval(session=sess,feed_dict={
       x:batch[0], y_:batch[1], keep_prob: 1.0})
-  print "step %d, training accuracy %g" %(i, train_accurancy)
-  train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+    print "step %d, training accuracy %g" %(i, train_accurancy)
+  train_step.run(session=sess, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-print "test accuracy %g" %accuracy.eval(feed_dict={
+print "test accuracy %g" %accuracy.eval(session=sess, feed_dict={
   x: mnist.test.images, y_: mnist.test.lables, keep_prob: 1.0})
-
+   
