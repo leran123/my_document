675f737263b5800304761393844b4a8083a6499f
leran123 <33862321@qq.com>
2017-07-10 21:58:12 +0800

    post

---
+++ nodejs/post.js
@@ -0,0 +1,16 @@
+var util = require('util');
+var http = require('http');
+var querystring = require('querystring');
+
+http.createServer(function(req, res) {
+	var post = '';
+	req.on('data', function(chunk) {
+		post += chunk;
+	});
+
+	req.on('end', function() {
+		post = querystring.parse(post);
+		res.end(util.inspect(post));
+	});
+
+}).listen(3000);
